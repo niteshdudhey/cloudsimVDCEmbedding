cac6ac839f7d474456abbb592409fd1b8b8e9b4c
nitesh dudhey <nit.dudhey@gmail.com>
2017-01-06 14:07:20 +0530

    commit test

--- src/main/java/cloudsimExtension/cloudsimVDCEmbedding/Constants.java
+++ src/main/java/cloudsimExtension/cloudsimVDCEmbedding/Constants.java
@@ -5,7 +5,7 @@ package cloudsimExtension.cloudsimVDCEmbedding;
 
 /**
  * @author nitesh
- *
+ * The global constants.
  */
 public final class Constants {
 	public enum switchType {ROOT(0), AGGREGATE(1), EDGE(2);
